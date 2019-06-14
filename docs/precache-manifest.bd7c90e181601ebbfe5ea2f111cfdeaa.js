6568d6d03ff0138aa0a8289af230f924741799cb
João Teixeira <jpntex@gmail.com>
2019-06-15 00:42:02 +0100

    Update demo page

---
+++ docs/precache-manifest.bd7c90e181601ebbfe5ea2f111cfdeaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-calendar/robots.txt"
+  },
+  {
+    "revision": "3f53e4f5d2897feecaf6",
+    "url": "/vue-calendar/js/chunk-vendors.d16d9385.js"
+  },
+  {
+    "revision": "80e78a183e331978e9f3",
+    "url": "/vue-calendar/js/app.0cf7cd6e.js"
+  },
+  {
+    "revision": "151534b84ae57432867fe17ed1bfbe56",
+    "url": "/vue-calendar/index.html"
+  },
+  {
+    "revision": "3f53e4f5d2897feecaf6",
+    "url": "/vue-calendar/css/chunk-vendors.5f3c27d8.css"
+  },
+  {
+    "revision": "80e78a183e331978e9f3",
+    "url": "/vue-calendar/css/app.4655d873.css"
+  },
+  {
+    "revision": "67945272732e2c8f8c077378789ee5cf",
+    "url": "/vue-calendar/core.css"
+  }
+];
